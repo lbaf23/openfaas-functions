2da0521b54e1f9dea2f8c61beb2478e9ac259172
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-10 09:13:46 +0800

    init

---
+++ gpuinfo/handler.py
@@ -0,0 +1,12 @@
+import torch
+
+def handle():
+    res = {
+        "is_available": torch.cuda.is_available(),
+        "device_name": torch.cuda.get_device_name(),
+        "device_count": torch.cuda.device_count(),
+        "current_device": torch.cuda.current_device(),
+        "max_memory_allocated": torch.cuda.max_memory_allocated(),
+
+    }
+    return res
